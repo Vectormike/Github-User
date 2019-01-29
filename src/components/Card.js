bd24983bf5c9bd644abdbd26c83d78d0bd8b54d7
Vectormike40 <victor.junior40@yahoo.com>
2019-01-29 16:21:56 +0100

    Created simple card component, awaiting props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 
-const Card = () => {
+const Card = ({id, name, followers, location}) => {
     return (
         <div className="">
-            <img src={``}/>
+            <img alt={`Avatar`} src={`https://avatars0.githubusercontent.com/u/${id}?v=4`}/>
             <hr/>
             <div>
-                <h3 className="f2"></h3>
-                <h3 className="f2"></h3>
-                <h3 className="f2"></h3>
+                <h3 className="f2">{name}</h3>
+                <h3 className="f2">{followers}</h3>
+                <h3 className="f2">{location}</h3>
             </div>
         </div>
     );
