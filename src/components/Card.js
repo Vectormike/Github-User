321535afca9593b76945c538976395d477d2f4d8
Vectormike40 <victor.junior40@yahoo.com>
2019-02-05 16:11:05 +0100

    Fixing card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 
-const Card = ({id, name, followers, location}) => {
-
+const Card = ({userID}) => {
+    const {id, name, followers, location} = userID;
     return (
         <div className="tc bg-light-blue grow br3 ma3 shadow-2">
             <img alt={`Avatar`} src={`https://avatars0.githubusercontent.com/u/${id}?v=4`}/>
             <hr/>
             <div className="tc">
-                <h3 className="f2">{name}</h3>
-                <h3 className="f2">{followers}</h3>
-                <h3 className="f2">{location}</h3>
+                <h3 className="f2">Name: {name}</h3>
+                <h3 className="f2">Followers: {followers}</h3>
+                <h3 className="f2">Location: {location}</h3>
             </div>
         </div>
     );
