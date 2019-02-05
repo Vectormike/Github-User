9bbf3bb534dd87e812f45bcd976db0aa0db1f0eb
Vectormike40 <victor.junior40@yahoo.com>
2019-02-05 16:30:01 +0100

    Fixed card 'dib'

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,13 @@ import React from 'react';
 const Card = ({userID}) => {
     const {id, name, followers, location} = userID;
     return (
-        <div className="tc bg-light-blue grow br3 ma3 shadow-2">
-            <img alt={`Avatar`} src={`https://avatars0.githubusercontent.com/u/${id}?v=4`}/>
-            <hr/>
+        <div className=" bg-light-red dib grow br3 ma  shadow-2">
+            <img className="br4 ma3" alt={`Avatar`} src={`https://avatars0.githubusercontent.com/u/${id}?v=4`}/>
+            <hr className="mw3 bb bw1 b--black-30"/>
             <div className="tc">
-                <h3 className="f2">Name: {name}</h3>
-                <h3 className="f2">Followers: {followers}</h3>
-                <h3 className="f2">Location: {location}</h3>
+                <h3 className="f3">Name: {name}</h3>
+                <h3 className="f3">Followers: {followers}</h3>
+                <h3 className="f3">Location: {location}</h3>
             </div>
         </div>
     );
