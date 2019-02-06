5370a35bc92d9a304c2d9186afa61199252501b9
Vectormike40 <victor.junior40@yahoo.com>
2019-02-06 16:11:13 +0100

    Added this component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <a href="#" className="grow dim tc mb2 mb0-ns" title="GitHub">
+                    <i className="dib w2 h2 fab fa-github"></i>
+                </a>
+        
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
