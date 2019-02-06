ee85b8f47d391128ae6b943e6d5294996c62488b
Vectormike40 <victor.junior40@yahoo.com>
2019-02-06 13:38:31 +0100

    Added onkeypress function 'I can'tcoman  kill myself'

--- src/components/App.js
+++ src/components/App.js
@@ -16,34 +16,30 @@ class App extends Component {
     componentDidMount() {
         this.getUser('vectormike40');
     }
-        //Fetch API with a parameter
-        getUser = (username) => {
-            fetch(`https://api.github.com/users/${username}`)
-            .then(response => response.json())
-            .then(userInfo => this.setState({user: {
-                id: userInfo.id,
-                name: userInfo.name,
-                followers: userInfo.followers,
-                location: userInfo.location}
-            })     
-        )}
-            
-    //Set current state when searched
-    onSearchChange = (event) => {
-        console.log(event.target.value)
-        this.setState({searchField: event.target.value})
-        this.getUser(event.target.value);
-    }
+    
+    //Fetch API with a parameter
+    getUser = (username) => {
+        fetch(`https://api.github.com/users/${username}`)
+        .then(response => response.json())
+        .then(userInfo => this.setState({user: {
+            id: userInfo.id,
+            name: userInfo.name,
+            followers: userInfo.followers,
+            location: userInfo.location}
+        })     
+    )}
+    onKeyPress = (event) => {
+		if(event.keyCode === 13 || event.charCode ===13) {
+			this.setState({searchField: event.target.value})
+			this.getUser(event.target.value)
+		}
+	}        
 
-    render() {
-        
-        //Allow user to search to allow filtering
-        
-        
+    render() {    
         return(
             <div className="tc">
-                <Search searchChange={this.onSearchChange}/>
-                <Card userID={this.state.user}/>
+                <Search keyPress={this.onKeyPress} search={this.state.searchField}/>
+				<Card userID={this.state.user}/>
                 <Footer/>
             </div>
         );
